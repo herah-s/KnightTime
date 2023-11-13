375bd90b8f03ebbb878993f2fef0dae907ea856a
Rina Iwamine <leddihome@gmail.com>
2023-11-13 18:09:06 +0900

    added status validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,5 @@ class Booking < ApplicationRecord
   validates :start_date, end_date: true
   validates :end_date, comparison: { greater_than: :start_date }
   validates :total_price, numericality: true
+  validates :status, inclusion: { in: ['pending', 'confirmed', 'rejected'] }
 end
