37f0af62468da62b7573f9e6e28eb1c5182585de
Rachael Momber <rlmomber@gmail.com>
2023-11-14 13:25:16 +0900

    wip

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :experience
 
-  validates :start_date, end_date: true
+  validates :start_date, :end_date, presence: true
   validates :end_date, comparison: { greater_than: :start_date }
   validates :total_price, numericality: true
   validates :status, inclusion: { in: ['pending', 'confirmed', 'rejected'] }
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -5,5 +5,5 @@ class Experience < ApplicationRecord
   validates :name, :description, :price, :address, :category, presence: true
   validates :description, length: { minimum: 100 }
   validates :price, numericality: true
-  validates :category, inclusion: { in: ["Jousting", "Archery", "Samurai", "Vikings"] }
+  validates :category, inclusion: { in: ["Jousting", "Archery", "Samurai", "Vikings", "Knights", "Ninja"] }
 end
