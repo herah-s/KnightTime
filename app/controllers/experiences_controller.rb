dc1b6327cb8502ca611dca78d2136fa896d7f129
Rachael Momber <rlmomber@gmail.com>
2023-11-14 18:05:12 +0900

    create index.html.erb for experiences

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,3 +1,11 @@
 class ExperiencesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @categories = ["Jousting", "Archery", "Samurai", "Vikings", "Knights", "Ninja"]
+    @experiences = Experience.all
+  end
+
+  def show
+  end
 end
