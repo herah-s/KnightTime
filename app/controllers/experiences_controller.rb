3cf8b7907027f36ed0fa68ffebf872abdc757b6a
Rina Iwamine <leddihome@gmail.com>
2023-11-14 18:23:14 +0900

    created the show page for an experience

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,3 +1,17 @@
 class ExperiencesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_experience, only: :show
+
+  def index
+  end
+
+  def show
+    @booking = Booking.new
+  end
+
+  private
+
+  def set_experience
+    @experience = Experience.find(params[:id])
+  end
 end
