2cdc153a97096b1bbb5eaccdcdc3e14215c5436b
Rina Iwamine <leddihome@gmail.com>
2023-11-16 11:23:35 +0900

    added update booking for host

--- app/controllers/host/bookings_controller.rb
+++ app/controllers/host/bookings_controller.rb
@@ -1,2 +1,17 @@
 class Host::BookingsController < ApplicationController
+  before_action :set_booking, only: :update
+  def update
+    @booking.update(booking_params)
+    @booking.save
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:status)
+  end
 end
