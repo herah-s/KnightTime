bc82614c720d2b869636d5f9c2c537da8c6eac71
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-11-16 16:06:49 +0900

    Host experiences page and Edit/Delete buttons done

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :bookings, only: [:index, :destroy]
 
   namespace :host do
+    resources :experiences, only: [:index, :destroy]
     resources :bookings, only: [:index, :update]
   end
 end
