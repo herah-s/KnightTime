c62963f02b678b754b20c85ae158ba70683ff639
Rina Iwamine <112476469+herah-s@users.noreply.github.com>
2023-12-10 19:51:39 +0900

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :booking, foreign_key: "knight_time_booking_id"
   belongs_to :user, foreign_key: "knight_time_user_id"
-  has_one :experience, through: :booking, foreign_key: "knight_time_experience_id"
-end
+  has_one :experience, through: :booking
