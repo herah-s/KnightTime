a54ebc7c7b2f58acc7026cbab0ce16864a51c697
Rina Iwamine <112476469+herah-s@users.noreply.github.com>
2023-12-10 19:45:09 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
-  belongs_to :booking
-  belongs_to :user
+  belongs_to :booking, foreign_key: "knight_time_booking_id"
+  belongs_to :user, foreign_key: "knight_time_user_id"
 end
