6dcc000d5ab4af8d59b12d5f9702d7b47dab9ea7
Rina Iwamine <112476469+herah-s@users.noreply.github.com>
2023-12-10 19:48:28 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :reviews, foreign_key: "knight_time_review_id"
-  has_many :bookings, foreign_key: "knight_time_booking_id"
+  has_many :reviews, foreign_key: "knight_time_user_id"
+  has_many :bookings, foreign_key: "knight_time_user_id"
   has_many :experiences_as_host, class_name: "KnightTimeExperience", foreign_key: :host_id
   has_many :bookings_as_host, class_name: "KnightTimeBooking", foreign_key: :host_id, through: :experiences_as_host, source: :bookings
   has_one_attached :photo
-  has_many :favorites, foreign_key: "knight_time_favorite_id"
+  has_many :favorites, foreign_key: "knight_time_user_id"
   has_many :favorited_experiences, through: :favorites, foreign_key: :knight_time_experience_id, source: :experience
 
   validates :first_name, presence: true
