010386557cf50ec1fe895555f993d410e1520713
Rina Iwamine <112476469+herah-s@users.noreply.github.com>
2023-12-10 19:53:21 +0900

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user, foreign_key: "knight_time_user_id"
   belongs_to :experience, foreign_key: "knight_time_experience_id"
-  has_one :review, , foreign_key: "knight_time_review_id"
+  has_one :review, foreign_key: "knight_time_booking_id"
   has_many :messages, dependent: :destroy, foreign_key: "knight_time_booking_id"
 
   validates :start_date, :end_date, presence: true
