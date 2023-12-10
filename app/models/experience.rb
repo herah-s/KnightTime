867d4e1fa70511d24d9b1cce928554724372a5de
Rina Iwamine <112476469+herah-s@users.noreply.github.com>
2023-12-10 19:39:00 +0900

    Update experience.rb

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,9 +1,10 @@
 class Experience < ApplicationRecord
-  belongs_to :host, class_name: "User"
-  has_many :bookings
-  has_many :reviews, through: :bookings
+  self.table_name = "knight_time_experiences"
+  belongs_to :host, class_name: "KnightTimeUser"
+  has_many :bookings, foreign_key: "knight_time_booking_id"
+  has_many :reviews, through: :bookings, foreign_key: "knight_time_review_id"
   has_one_attached :photo
-  has_many :favorites
+  has_many :favorites, foreign_key: "knight_time_favorite_id"
 
   validates :name, :description, :price, :address, :category, presence: true
   validates :description, length: { minimum: 10 }
