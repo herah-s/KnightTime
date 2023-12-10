f81e73090b92dd9100d3702c4b6d056d7aab07e0
Rina Iwamine <112476469+herah-s@users.noreply.github.com>
2023-12-10 20:00:17 +0900

    Update favorite.rb

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ApplicationRecord
+  self.table_name = "knight_time_favorites"
   belongs_to :user, foreign_key: "knight_time_user_id"
   belongs_to :experience, foreign_key: "knight_time_experience_id"
 end
