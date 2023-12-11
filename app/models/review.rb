c0ae81e2c4112d3249ed157a588e988e97433752
Rina Iwamine <leddihome@gmail.com>
2023-12-11 14:43:58 +0900

    fixed syntax error in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,3 +3,4 @@ class Review < ApplicationRecord
   belongs_to :booking, foreign_key: "knight_time_booking_id"
   belongs_to :user, foreign_key: "knight_time_user_id"
   has_one :experience, through: :booking
+end
